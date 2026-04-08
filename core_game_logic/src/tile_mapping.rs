b4cfb76e62b88b1622085d87c866cb4787ea5e8e
logic-geek <79881103+logic-geek@users.noreply.github.com>
2026-04-08 16:47:10 -0400

    undid last change
    The last change replaced a loop with simple math, but that math relied on floats. If I can find a way do the math without floats in the future, great, but for now I'm settling for consistency.

--- core_game_logic/src/tile_mapping.rs
+++ core_game_logic/src/tile_mapping.rs
@@ -71,7 +71,15 @@ impl From<TileId> for HexVector2d {
 
         let id = id.0 as i32;
 
-        let ring = (0.5 + ((4 * id - 1) as f32 / 12.0).powf(0.5)).floor() as i32;
+        let ring = {
+            let mut examined_ring = 1;
+            loop {
+                if id <= 3 * (examined_ring + 1) * examined_ring {
+                    break examined_ring;
+                }
+                examined_ring += 1;
+            }
+        };
 
         let steps_on_ring = id - (3 * ring * (ring - 1) + 1);
 
@@ -135,7 +143,7 @@ mod tests {
 
     #[test]
     fn tile_conversions() {
-        for id in 0..10000000 {
+        for id in 0..10000 {
             let start_id = TileId::new(id);
 
             let end_id: TileId = HexVector2d::from(start_id).into();
