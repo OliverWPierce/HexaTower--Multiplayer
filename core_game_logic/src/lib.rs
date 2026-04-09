98ad89f1c470789293cef6af89fc13885e532971
logic-geek <79881103+logic-geek@users.noreply.github.com>
2026-04-08 21:16:52 -0400

    began logical tiles

--- core_game_logic/src/lib.rs
+++ core_game_logic/src/lib.rs
@@ -1,1 +1,23 @@
+pub use bevy::*;
+
+use crate::tiles::initialize_tiles;
 pub mod tile_mapping;
+pub mod tiles;
+
+pub struct CreationSettings {
+    board_size: u32,
+}
+
+impl CreationSettings {
+    pub fn new(board_size: u32) -> CreationSettings {
+        CreationSettings { board_size }
+    }
+
+    pub fn create_board(self) -> bevy::ecs::world::World {
+        let mut logical_world = bevy::ecs::world::World::new();
+
+        initialize_tiles(&mut logical_world, self.board_size);
+
+        logical_world
+    }
+}
---
+++ core_game_logic/src/tiles.rs
@@ -0,0 +1,25 @@
+use bevy::ecs::{
+    component::{Component, Immutable, StorageType},
+    entity::Entity,
+    resource::Resource,
+    world::World,
+};
+
+use crate::tile_mapping::TileId;
+
+pub fn initialize_tiles(world: &mut World, ring_count: u32) {
+    let ordered_tiles = world
+        .spawn_batch((0..(3 * (ring_count + 1) * ring_count + 1)).map(TileId::new))
+        .collect::<Vec<Entity>>();
+
+    world.insert_resource(TileDirectory(ordered_tiles));
+}
+
+#[derive(Debug, Resource)]
+pub struct TileDirectory(Vec<Entity>);
+
+impl Component for TileId {
+    const STORAGE_TYPE: bevy::ecs::component::StorageType = StorageType::Table;
+
+    type Mutability = Immutable;
+}
