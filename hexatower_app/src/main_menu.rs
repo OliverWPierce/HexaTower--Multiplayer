b0855d9ef56a01fa640f73cedee9dcd745ed6a48
logic-geek <79881103+logic-geek@users.noreply.github.com>
2026-07-10 11:49:03 -0400

    IP address and name collection nodes

--- hexatower_app/src/main_menu.rs
+++ hexatower_app/src/main_menu.rs
@@ -1,5 +1,6 @@
-use bevy::color::palettes::tailwind::*;
 use bevy::prelude::*;
+use bevy::text::EditableText;
+use bevy::{color::palettes::tailwind::*, text::TextCursorStyle};
 
 use crate::{
     AppState,
@@ -12,8 +13,14 @@ pub struct MainMenuAndLobbyPluggin;
 impl Plugin for MainMenuAndLobbyPluggin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::MainMenu), render_main_menu);
+        app.add_systems(
+            OnEnter(AppState::ParametersScreen),
+            render_parameters_screen,
+        );
     }
 }
+
+const HEADER_SIZE: FontSize = FontSize::Vh(6.0);
 
 fn render_main_menu(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -29,13 +36,14 @@ fn render_main_menu(mut commands: Commands) {
                 ..default()
             },
             BackgroundColor(UNIVERSAL_BACKGROUND),
+            MenusBackgroundNode,
         ))
         .id();
 
     commands.spawn((
         ChildOf(source_node),
         Text::new("Hexatower"),
-        TextFont::from_font_size(FontSize::Vh(15.0)),
+        TextFont::from_font_size(HEADER_SIZE),
     ));
 
     const BUTTON_TEXT_SIZE: FontSize = FontSize::Vh(6.0);
@@ -64,7 +72,7 @@ fn render_main_menu(mut commands: Commands) {
         .spawn((
             button_bundle.clone(),
             children![(
-                Text::new("Join Online Game"),
+                Text::new("Join Game"),
                 TextFont::from_font_size(BUTTON_TEXT_SIZE),
             )],
         ))
@@ -81,7 +89,7 @@ fn render_main_menu(mut commands: Commands) {
         .spawn((
             button_bundle.clone(),
             children![(
-                Text::new("Host Online Game"),
+                Text::new("Host and Play"),
                 TextFont::from_font_size(BUTTON_TEXT_SIZE),
             )],
         ))
@@ -106,3 +114,107 @@ fn render_main_menu(mut commands: Commands) {
             commands.write_message(AppExit::Success);
         });
 }
+
+#[derive(Debug, Component)]
+struct MenusBackgroundNode;
+
+fn render_parameters_screen(
+    networking_mode: Res<MultiplayerNetworkingMode>,
+    background_node: Single<Entity, With<MenusBackgroundNode>>,
+    mut commands: Commands,
+) {
+    commands.entity(background_node.entity()).despawn_children();
+
+    match *networking_mode {
+        MultiplayerNetworkingMode::SingleDevice => todo!(),
+        MultiplayerNetworkingMode::Host => {
+            commands.spawn((
+                ChildOf(background_node.entity()),
+                Text::new("Host Game"),
+                TextFont::from_font_size(HEADER_SIZE),
+            ));
+
+            commands.spawn((
+                Node {
+                    width: Val::Percent(25.0),
+                    height: Val::Vh(7.0),
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ChildOf(background_node.entity()),
+                children![
+                    (
+                        Text::new("IP address:"),
+                        TextFont::from_font_size(FontSize::Vh(2.0)),
+                    ),
+                    (
+                        Node {
+                            width: Val::Percent(100.0),
+                            border: px(2).all(),
+                            ..Default::default()
+                        },
+                        IpAdressCollectionNode,
+                        BorderColor::from(Color::from(SLATE_700)),
+                        EditableText {
+                            visible_width: Some(10.),
+                            allow_newlines: false,
+                            max_characters: Some(25),
+                            ..Default::default()
+                        },
+                        TextLayout::no_wrap(),
+                        TextFont {
+                            font_size: FontSize::Vh(4.0),
+                            ..default()
+                        },
+                        TextCursorStyle::default(),
+                        BackgroundColor(SLATE_800.into()),
+                    )
+                ],
+            ));
+
+            commands.spawn((
+                Node {
+                    width: Val::Percent(25.0),
+                    height: Val::Vh(7.0),
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ChildOf(background_node.entity()),
+                children![
+                    (
+                        Text::new("Display name. Enter a title fit for a king."),
+                        TextFont::from_font_size(FontSize::Vh(2.0)),
+                    ),
+                    (
+                        Node {
+                            width: Val::Percent(100.0),
+                            border: px(2).all(),
+                            ..Default::default()
+                        },
+                        GamertagCollectionNode,
+                        BorderColor::from(Color::from(SLATE_700)),
+                        EditableText {
+                            visible_width: Some(10.),
+                            allow_newlines: false,
+                            max_characters: Some(15),
+                            ..Default::default()
+                        },
+                        TextLayout::no_wrap(),
+                        TextFont {
+                            font_size: FontSize::Vh(4.0),
+                            ..default()
+                        },
+                        TextCursorStyle::default(),
+                        BackgroundColor(SLATE_800.into()),
+                    )
+                ],
+            ));
+        }
+        MultiplayerNetworkingMode::Client => todo!(),
+    }
+}
+
+#[derive(Debug, Component)]
+struct IpAdressCollectionNode;
+#[derive(Debug, Component)]
+struct GamertagCollectionNode;
